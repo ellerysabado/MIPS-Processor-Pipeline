55b10cb529fe065595980260e38055619014477a
elsabado <elsabado@co2050-11.ece.iastate.edu>
2023-11-01 15:48:03 -0500

    IF_ID added

---
+++ src/TopLevel/IF_ID.vhd
@@ -0,0 +1,46 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity IF_ID is
+  generic(N : integer := 32);
+  port(i_CLK        : in std_logic;     -- Clock input
+       i_RST        : in std_logic;     -- Reset input
+       i_WE         : in std_logic;     -- Write enable input
+       i_PCAdd      : in std_logic_vector(N-1 downto 0);     -- Data value input
+       i_InstMem    : in std_logic_vector(N-1 downto 0);     -- Data value input 
+       o_PCAdd      : out std_logic_vector(N-1 downto 0);     -- Data value output FIX THIS Data bits--------------------------------
+       o_InstMem    : out std_logic_vector(N-1 downto 0));    -- Data value output 
+
+end IF_ID;
+
+architecture dataflow of IF_ID is
+
+component Register_N
+
+  port(i_CLK        : in std_logic;     -- Clock input
+       i_RST        : in std_logic;     -- Reset input
+       i_WE         : in std_logic;     -- Write enable input
+       i_D          : in std_logic_vector(N-1 downto 0);     -- Data value input
+       o_Q          : out std_logic_vector(N-1 downto 0));   -- Data value output
+
+end component;
+
+
+begin
+
+  PCAdd : Register_N
+	port MAP(i_CLK           => i_CLK,
+		       i_RST           => i_RST,
+           i_WE            => i_WE,
+		       i_D             => i_PCAdd,
+           o_Q             => o_PCAdd); 
+
+
+  InstMem : Register_N
+	port MAP(i_CLK           => i_CLK,
+		       i_RST           => i_RST,
+           i_WE            => i_WE,
+		       i_D             => i_InstMem,
+           o_Q             => o_InstMem); 
+
+end dataflow;
