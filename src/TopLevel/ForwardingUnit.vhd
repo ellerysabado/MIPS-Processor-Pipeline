36309045eeb9c1dd427af04778aa6b68deeb1996
elsabado <elsabado@co2050-12.ece.iastate.edu>
2023-11-08 14:52:13 -0600

    forward units update

--- src/TopLevel/ForwardingUnit.vhd
+++ src/TopLevel/ForwardingUnit.vhd
@@ -19,16 +19,30 @@ end entity ForwardingUnit;
 architecture Behavioral of ForwardingUnit is
    begin
       -- Hazard detection and control logic for ForwardA
-      ForwardA <= "00" when (EX_MEM_RegWrite = '0' or EX_MEM_RegisterRd = "00000") else
-                  "10" when (EX_MEM_RegWrite = '1' and EX_MEM_RegisterRd = ID_EX_RegisterRs) else
+      ForwardA <= "01" when (MEM_WB_RegWrite = '1'
+                       and (MEM_WB_RegisterRd /= "00000") 
+                       and not(EX_MEM_RegWrite = '1' and (EX_MEM_RegisterRd /= "00000") 
+                           and (EX_MEM_RegisterRd = ID_EX_RegisterRs)) 
+                       and (MEM_WB_RegisterRd = ID_EX_RegisterRs)) else
+                  "10" when (EX_MEM_RegWrite = '1'
+                       and (EX_MEM_RegisterRd /= "00000")
+                       and (EX_MEM_RegisterRd = ID_EX_RegisterRs)) else
                   "00";
+      
+
    
       -- Hazard detection and control logic for ForwardB
-      ForwardB <= "00" when (EX_MEM_RegWrite = '0' or EX_MEM_RegisterRd = "00000") else
-                  "10" when (EX_MEM_RegWrite = '1' and EX_MEM_RegisterRd = ID_EX_RegisterRt) else
+      ForwardB <= "01" when (MEM_WB_RegWrite = '1'
+                       and (MEM_WB_RegisterRd /= "00000") 
+                       and not(EX_MEM_RegWrite = '1' and (EX_MEM_RegisterRd /= "00000") 
+                           and (EX_MEM_RegisterRd = ID_EX_RegisterRt)) 
+                       and (MEM_WB_RegisterRd = ID_EX_RegisterRt)) else
+                  "10" when (EX_MEM_RegWrite = '1'
+                       and (EX_MEM_RegisterRd /= "00000")
+                       and (EX_MEM_RegisterRd = ID_EX_RegisterRt)) else
                   "00";
    
-      -- Similar logic for MEM_WB stage if needed
+
    
 end Behavioral;
    
